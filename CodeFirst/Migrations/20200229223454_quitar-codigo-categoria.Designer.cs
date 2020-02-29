e30dbd919557d1383544854a3cffd2c832e8e48f
Adrian Radice <radice.adrian.sjt@gmail.com>
2020-02-29 19:35:27 -0300

    quitar campo codigo de categoria

--- CodeFirst/Migrations/20200229223454_quitar-codigo-categoria.Designer.cs
+++ CodeFirst/Migrations/20200229223454_quitar-codigo-categoria.Designer.cs
@@ -4,14 +4,16 @@ using CodeFirst.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CodeFirst.Migrations
 {
     [DbContext(typeof(DbContexto))]
-    partial class DbContextoModelSnapshot : ModelSnapshot
+    [Migration("20200229223454_quitar-codigo-categoria")]
+    partial class quitarcodigocategoria
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- CodeFirst/Models/Categoria.cs
+++ CodeFirst/Models/Categoria.cs
@@ -10,7 +10,6 @@ namespace CodeFirst.Models
         }
 
         public int IdCategoria { get; set; }
-        public string Codigo { get; set; }
         public string Nombre { get; set; }
         public string Descripcion { get; set; }
         public bool? Estado { get; set; }
